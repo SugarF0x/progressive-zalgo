c58cddef4847a9c157e02ead8e92ab3171dbdf3c
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-05-01 10:26:01 +0300

    feat: initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="background">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.background {
+  background: rgb(251, 191, 36) linear-gradient(transparent, red);
+
+  @apply h-screen w-screen flex justify-center items-center
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  ssr: false,
+  modules: ['@nuxtjs/tailwindcss']
+})

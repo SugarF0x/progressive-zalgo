b44c4cf0767e488158516d2ef7f03ccd8528647d
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-05-01 22:37:18 +0300

    dev: add icon & title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'HE COMES',
+      link: [{ rel: "icon", type: "image/x-icon", href: "/icon.png" }]
+    }
+  },
   ssr: false,
   modules: ['@nuxtjs/tailwindcss']
 })
